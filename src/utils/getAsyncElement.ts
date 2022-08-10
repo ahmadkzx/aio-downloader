9b15ef1d94f0becfd66058265ac1859be3067a79
Ahmad Karimzade <ahmadkarimzadedev@gmail.com>
2022-08-10 14:12:01 +0430

    fix: promise resolve type in getAsyncElement util

--- src/utils/getAsyncElement.ts
+++ src/utils/getAsyncElement.ts
@@ -1,11 +1,12 @@
 export default function getAsyncElement(selector: string) {
-  return new Promise((resolve) => {
-    if (document.querySelector(selector)) return resolve(document.querySelector(selector))
+  return new Promise<HTMLElement>((resolve) => {
+    if (document.querySelector(selector))
+      return resolve(document.querySelector(selector) as HTMLElement)
 
     const observer = new MutationObserver(() => {
       if (document.querySelector(selector)) {
         observer.disconnect()
-        resolve(document.querySelector(selector))
+        resolve(document.querySelector(selector) as HTMLElement)
       }
     })
 
