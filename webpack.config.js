d7fc7f1ef8ccdf78bc914879fab4bdb42c12bc1b
Ahmad Karimzade <ahmadkarimzadedev@gmail.com>
2022-08-10 14:03:01 +0430

    config: webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+
+  entry: {
+    instagram: './src/instagram/index.ts',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+}
