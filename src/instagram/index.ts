ba89aa11ea36fb58092316ba5f01141992dcb4c2
Ahmad Karimzade <ahmadkarimzadedev@gmail.com>
2022-08-12 22:48:38 +0430

    feat: inject instagram dl btn to html

--- src/instagram/index.ts
+++ src/instagram/index.ts
@@ -1,11 +1,13 @@
 import getAsyncElement from '../utils/getAsyncElement'
+import './instagram.scss'
 
 async function injectDownloadBtn() {
   const btn = document.createElement('button')
+  btn.id = 'unique'
+  btn.style.backgroundImage = `url(${chrome.runtime.getURL('dist/static/instagram-download.svg')})`
 
-  const container = await getAsyncElement('._aamu _aaz9')
-  console.log(container)
-  // container.appendChild(btn)
+  const container = await getAsyncElement('._aamu')
+  container.appendChild(btn)
 }
 
 injectDownloadBtn()
