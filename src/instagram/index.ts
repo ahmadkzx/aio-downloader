f4d0768a4845badd55b80502f66f215eee014482
Ahmad Karimzade <ahmadkarimzadedev@gmail.com>
2022-08-13 11:41:17 +0430

    feat: download current slide image

--- src/instagram/index.ts
+++ src/instagram/index.ts
@@ -1,6 +1,30 @@
 import getAsyncElement from '../utils/getAsyncElement'
 import './instagram.scss'
 
+const CLASS_LIST = {
+  paginationDot: '._acnb',
+  slidesContainer: '._acay',
+  actionsContainer: '._aamu',
+  paginationNextBtn: '._aahi',
+  paginationPrevBtn: '._aahh',
+}
+
+let slidesCount = 0
+let currentSlideIndex = 0
+
+async function preparePage() {
+  const nextBtn = await getAsyncElement(CLASS_LIST.paginationNextBtn)
+  nextBtn.addEventListener('click', async () => {
+    currentSlideIndex++
+    if (currentSlideIndex == 1) {
+      const prevBtn = await getAsyncElement(CLASS_LIST.paginationPrevBtn)
+      prevBtn.addEventListener('click', () => currentSlideIndex--)
+    }
+  })
+
+  slidesCount = document.querySelectorAll(CLASS_LIST.paginationDot).length
+}
+
 async function injectDownloadBtn() {
   const btn = document.createElement('button')
   btn.id = 'unique'
@@ -8,15 +32,27 @@ async function injectDownloadBtn() {
 
   btn.addEventListener('click', download)
 
-  const container = await getAsyncElement('._aamu')
+  const container = await getAsyncElement(CLASS_LIST.actionsContainer)
   container.appendChild(btn)
 }
 
 function download() {
-  const imgEl = document.getElementsByClassName('_aagt')[0] as HTMLImageElement
+  let imgIndex
+  if (currentSlideIndex == 0) {
+    imgIndex = 2
+  } else if (currentSlideIndex == slidesCount) {
+    imgIndex = 4
+  } else {
+    imgIndex = 3
+  }
+
+  const imgEl = document.querySelector(
+    `${CLASS_LIST.slidesContainer} li:nth-child(${imgIndex}) img`
+  ) as HTMLImageElement
   const imgUrl = imgEl.srcset.split(',')[0].replace('1080w', '')
 
   window.open(imgUrl, '__blank')
 }
 
+preparePage()
 injectDownloadBtn()
